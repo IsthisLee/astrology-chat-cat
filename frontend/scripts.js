1b9b50e0657a60f4ba3ffd3a1d01f6edca1d434b
IsthisLee <rjsgmldnwn@gmail.com>
2023-05-01 05:16:43 +0900

    chore: Add toss pay link in chat

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -18,7 +18,7 @@ async function sendMessage() {
 
   if (messageText.length === 0) return;
 
-  addChatMessage("user-message", messageText);
+  addUserMessage(messageText);
   // Memory에 유저 대화 내역 저장
   userMessages.push(messageText);
 
@@ -44,39 +44,58 @@ async function sendMessage() {
     ).then((res) => res.json());
 
     if (response.success) {
-      addChatMessage("assistant-message", response.data.assistant);
+      addAssistantMessage(response.data.assistant);
       afterGetChat();
 
       // Memory에 챗냐옹 대화 내역 저장
       assistantMessages.push(response.data.assistant);
     } else {
-      addChatMessage(
-        "assistant-message",
-        `운세 요청에 실패했다냐옹. ${response.message}`
-      );
+      addAssistantMessage(`운세 요청에 실패했다냐옹. ${response.message}`);
       afterGetChat();
 
       userMessages.pop(); // 챗냐옹 응답 실패 시 마지막 유저 대화 내역 삭제
     }
   } catch (error) {
-    addChatMessage("assistant-message", "네트워크 오류");
+    addAssistantMessage("네트워크 오류");
     userMessages.pop(); // 챗냐옹 응답 실패 시 마지막 유저 대화 내역 삭제
   }
 }
 
-function addChatMessage(className, text) {
+function addAssistantMessage(text) {
   const chatTextContainer = document.getElementById("chatTextContainer");
   const newMessage = document.createElement("div");
-  newMessage.className = `message ${className}`;
+  newMessage.className = "message assistant-message";
 
   const messageContent = document.createElement("div");
   messageContent.className = "message-content";
   messageContent.textContent = text;
+
+  if (assistantMessages.length >= 1) {
+    const p = document.createElement("p");
+    p.innerHTML =
+      "그리고 참치캔 값을 내놓으면 더욱 좋은 운이 찾아올 것이다냐옹 => ";
+    const link = document.createElement("a");
+    link.href = "https://toss.me/comecome";
+    link.innerHTML = "참치캔 값 내놓기";
+    p.appendChild(link);
+    messageContent.appendChild(p);
+  }
   newMessage.appendChild(messageContent);
 
-  const messageTail = document.createElement("div");
-  messageTail.className = "message-tail";
-  newMessage.appendChild(messageTail);
+  chatTextContainer.appendChild(newMessage);
+  chatTextContainer.scrollTop = chatTextContainer.scrollHeight;
+}
+
+function addUserMessage(text) {
+  const chatTextContainer = document.getElementById("chatTextContainer");
+  const newMessage = document.createElement("div");
+  newMessage.className = "message user-message";
+
+  const messageContent = document.createElement("div");
+  messageContent.className = "message-content";
+  messageContent.textContent = text;
+
+  newMessage.appendChild(messageContent);
 
   chatTextContainer.appendChild(newMessage);
   chatTextContainer.scrollTop = chatTextContainer.scrollHeight;
@@ -116,7 +135,7 @@ const selectBox = document.getElementById("bornHour");
 for (let i = -1; i <= 23; i++) {
   let option = document.createElement("option");
   option.value = i;
-  option.text = i;
+  option.text = `${i} 시`;
 
   if (i === -1) {
     option.value = null;
@@ -133,7 +152,6 @@ document.getElementById("inputText").addEventListener("keypress", (event) => {
   }
 });
 
-addChatMessage(
-  "assistant-message",
+addAssistantMessage(
   "냐옹, 나는 운세보는 냐옹이다냐옹. 궁금한 것을 물어보라냐옹! (ex. 오늘 운세 알려줘라냐옹)"
 );
